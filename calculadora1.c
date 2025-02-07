83e060d151674d7149d0e059ad9ed1143113c34c
YanGleidson <139282435+YanGleidson@users.noreply.github.com>
2025-02-07 20:39:55 -0300

    Update calculadora1.c

--- calculadora1.c
+++ calculadora1.c
@@ -13,7 +13,7 @@ float calculadora(float num1, float num2, int operacao) {
             return num1 * num2;
         case 4:
             if (num2 == 0) {
-                printf("Divis„o por zero n„o È permitida!\n");
+                printf("Divis√£o por zero n√£o √© permitida!\n");
                 return 0;
             } else {
                 return num1 / num2;
@@ -29,13 +29,13 @@ int main(){
     float numero1, numero2, resultado;
     int operacao, soma, sub, multi, divi;
 
-    printf("ESCOLHA A OPERA«√O: (1-SOMA, 2-SUBTRA«√O, 3-MULTIPLICA«√O, 4-DIVIS√O): \n");
+    printf("ESCOLHA A OPERA√á√ÉO: (1-SOMA, 2-SUBTRA√á√ÉO, 3-MULTIPLICA√á√ÉO, 4-DIVIS√ÉO): \n");
     scanf("%d", &operacao);
 
-    printf("Digite o Primeiro N˙mero: ");
+    printf("Digite o Primeiro N√∫mero: ");
     scanf("%f", &numero1);
 
-    printf("Digite o Segundo N˙mero: ");
+    printf("Digite o Segundo N√∫mero: ");
     scanf("%f", &numero2);
 
     resultado = calculadora(numero1, numero2, operacao);
